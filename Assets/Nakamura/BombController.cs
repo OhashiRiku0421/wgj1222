ecb7d015b639f6864834827297f5885f3450d4f8
e-mau <45002974+e-mau@users.noreply.github.com>
2022-12-22 16:00:13 +0900

    [add] 爆弾のコントローラーコンポーネントを作成

---
+++ Assets/Nakamura/BombController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UniRx;
+using System;
+
+public class BombController : MonoBehaviour
+{
+    [SerializeField] StickyMovementController m_movementController;
+    [SerializeField] BomMessagePublisher m_messagePublisher;
+
+    private void Start()
+    {
+        m_movementController.OnSticked.Skip(1).Subscribe(_ => { m_messagePublisher.Publish(); Debug.Log("a"); }).AddTo(this);
+    }
+}
